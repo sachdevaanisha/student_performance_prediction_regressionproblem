85968290de687d1c17d87a432918e7ae03aec8d6
sachdevaanisha <sachdeva.anisha@yahoo.com>
2024-08-27 23:22:06 -0400

    added initial analysis jupyter notebook

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+
+    return requirements
+
+setup(
+    name='student_performance_prediction_regressionproblem',
+    version='0.0.1',
+    author='Anisha Sachdeva',
+    author_email='sachdeva.anisha@yahoo.com',
+    packages=find_packages(),
+    install_requires = get_requirements('requirements.txt'))
